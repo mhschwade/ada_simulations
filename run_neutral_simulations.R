221c61147b43d08de7a8e251f72360424cd19298
mhschwade <marceloh.schwade@gmail.com>
2024-07-01 18:10:35 -0300

    Improve the readability of the  script and add instructions to use the scripts to run the neutral simulations.

--- run_neutral_simulations.R
+++ run_neutral_simulations.R
@@ -38,25 +38,28 @@ require(Rcpp)
 # Get simulation path.
 simulation_path <- "/Users/marceloschwade/Desktop/ada_simulations"
 
+# Define the config file of the simulation.
+config_file <- file.path(simulation_path, "config", "config6_cpp.R")
+
+# Defining the basic paths to landscape and to save the results of the simulation.
+landscape_path <- file.path(simulation_path, "landscapes_20231114", "landscapes", "distances")
+output_directory <- file.path(simulation_path, "neutral_simulations")
+
+# Number cores to use to parallelize the simulations.
+no_cores <- 2
 
 ################################################################################
 # Import config file and check its integrity.
-
-# Creates a config object from config file.
-config_file <- file.path(simulation_path, "config", "config6_cpp.R")
 config_object <- create_input_config(config_file)
 
 # Before to start a simulation, we can verify if the config object works.
 verify_config(config_object)
 # If this verification returns TRUE it is good to go.
 
-# Defining other basic paths.
-landscape_path <- file.path(simulation_path, "landscapes_20231114", "landscapes", "distances")
-output_directory <- file.path(simulation_path, "neutral_simulations")
-
 ################################################################################
 # Function to run simulations using a set of parameters.
-# Function to run gen3sis changing the random seed.
+# Function to run gen3sis changing the random seed, the initial range and other 
+# basic parameters.
 runGen3sisSim <- function(config_file, landscape_path, output_directory, par, save_state=NA, verbose=T){
   
   config <- create_input_config(config_file)
@@ -86,28 +89,29 @@ runGen3sisSim <- function(config_file, landscape_path, output_directory, par, sa
   return(simulation_results)
 }
 
-
-# Run the simulations!!!
 ################################################################################
-# Finally, we can run a simulation with the landscape and the configuration that
-# we define. 
-####################################
+# Defining the parameters for the simulations.
 
-# Defining parameters to simulation.
-# Random seeds.
+# Random seeds, we are using 5 different random seeds.
 rseeds <- c(344062, 953111, 329969, 575612, 364862)
 
+# Initial range of the ancestor species, we are using 5 different initial ranges.
 range_initial <- list(c(-5, 0, -5, 0), 
                       c(8, 13, -10, -5),
                       c(-3, 2, -17, -12),
                       c(-12, -7, -24, -19),
                       c(10, 15, -30, -25))
-
 names(range_initial) <- c("A", "B", "C", "D", "E")
 
+# Dispersal rate (dispersal), niche breath (ecology, it doesn't have effect in 
+# neutral simulations) and trait variability (evolution).
 dispersal_rates <- c(0.6)
 niche_breaths <- c(0.05) # This parameter is not in use for neutral dynamics.
 trait_variability <- 0.01
+
+# The number of simulations is given by the multiplicity of the parameters.
+# How only `rseeds` and `range_initial` have more than 1 'value'/state, then the 
+# number of simulations is merely 5x5 = 25 simulations.
 
 # Put all set of parameters into a list. Each set of parameters (element of the 
 # list) is used to run a simulation.
@@ -130,23 +134,27 @@ for(rseed in rseeds){
 }
 
 
+# Run the simulations!!!
+################################################################################
+# Finally, we can run a simulation with the landscape and the configuration that
+# we define. 
+####################################
+
 # Run one simulation to test if the script is working.
 sim.results <- runGen3sisSim(config_file, landscape_path, output_directory, pars[[1]], save_state=NA)
 
-
 # Run all the missing simulations using parallelization.
-# Number cores to use.
-no_cores <- 12 # For Monolito.
-
+# Create the cluster to run the simulations in parallel using no_cores.
 cl <- makeCluster(no_cores)
 registerDoParallel(cl)
 
 # Run the simulations in parallel. Each simulation run in a core.
 foreach(i=2:nsim, .packages=c('gen3sis', 'raster', 'Rcpp')) %dopar% {
-  runGen3sisSim(config_file, landscape_path, output_directory, pars[[i]], verbose=F)
+  sim.results.par <- runGen3sisSim(config_file, landscape_path, output_directory, pars[[i]], verbose=F)
 }
 
 stopCluster(cl)
 
 # Task finished.
-cat("This script has finished its execution.")
+cat("This script has finished its execution.\n")
+cat(paste("The simulation results can be found into the path: ", output_directory, "\n"))
